5a0a2b2a9e024ae76e47337ee7ae0f1c41bf325e
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-19 01:27:17 +0530

    patterns done like inverted half pyramid, numeric half pyramid & inverted numeric  half pyramid

--- pattern_level_1.cpp
+++ pattern_level_1.cpp
@@ -55,11 +55,72 @@ int main(){
     // * * * * 
     // * * * * *
 
+    // int n;
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<row-1; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // * * * * *
+    // * * * *
+    // * * *
+    // * *
+    // * 
+
+    // int n; 
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<n-row; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+
+    // int n;
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<row+1; col++){
+    //         cout<<col+1<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 5 4 3 2 1
+    // 5 4 3 2
+    // 5 4 3
+    // 5 4
+    // 5
+
+    // int n;
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<n-row; col++){
+    //         cout<<n-col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 1 2 3 4 5
+    // 1 2 3 4
+    // 1 2 3
+    // 1 2 
+    // 1
+
     int n;
     cin>>n;
     for(int row=0; row<n; row++){
-        for(int col=0; col<row+1; col++){
-            cout<<"* ";
+        for(int col=0; col<n-row; col++){
+            cout<<col+1<<" ";
         }
         cout<<endl;
     }
