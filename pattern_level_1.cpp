0088595c90484084ce35068ef3d5aa9e7e0f5b9f
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-19 00:34:13 +0530

    patterns done like square, rectangle, hollow rectangle & half pyramid

--- pattern_level_1.cpp
+++ pattern_level_1.cpp
@@ -33,18 +33,33 @@ int main(){
     //  *       *
     //  * * * * *
 
-    int ROWS =10;
-    int COLS = 7;
-    for(int row=0; row<ROWS; row++){
-        for(int col=0; col<COLS; col++){
-            if(row == 0 || row == ROWS-1)
+    // int ROWS =10;
+    // int COLS = 7;
+    // for(int row=0; row<ROWS; row++){
+    //     for(int col=0; col<COLS; col++){
+    //         if(row == 0 || row == ROWS-1)
+    //         cout<<"* ";
+    //         else{
+    //             if(col==0 || col==COLS-1)
+    //             cout<<"* ";
+    //             else
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *
+    // * *
+    // * * *
+    // * * * * 
+    // * * * * *
+
+    int n;
+    cin>>n;
+    for(int row=0; row<n; row++){
+        for(int col=0; col<row+1; col++){
             cout<<"* ";
-            else{
-                if(col==0 || col==COLS-1)
-                cout<<"* ";
-                else
-                cout<<"  ";
-            }
         }
         cout<<endl;
     }
