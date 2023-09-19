d99a5b1eece8078a562970830a1635607ad9813f
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-19 13:18:57 +0530

    hollow pyramid , inverted hollow pyramid & hollow diamond

--- pattern_level_2.cpp
+++ pattern_level_2.cpp
@@ -40,28 +40,119 @@ int main(){
     //     cout<<endl;
     // }
 
+    //    * 
+    //   * * 
+    //  * * * 
+    // * * * * 
+    // * * * * 
+    //  * * * 
+    //   * *
+    //    *
+    // int num; 
+    // cin>>num;
+    // int n=num/2;
+    // for(int row=0; row<n; row++){
+    //     //space
+    //     for(int col=0; col<n-row-1; col++){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0; col<row+1; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    //     //start
+    // }
 
-    int num; 
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<row; col++){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0; col<n-row; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //     * 
+    //    * * 
+    //   *   * 
+    //  *     * 
+    // *       * 
+    // int n;
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<n-row-1; col++){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0; col<row+1; col++){
+    //         if(col==0 || col==row+1-1){
+    //             cout<<"* ";
+    //         }else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    // *       *
+    //  *     *
+    //   *   *
+    //    * *
+    //     *
+    // int n; 
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<row; col++){
+    //         cout<<" ";
+    //     }
+    //     int totalCol = n-row;
+    //     for(int col=0; col<totalCol; col++){
+    //         if(col==0 || col==totalCol-1){
+    //             cout<<"* ";
+    //         }else{
+    //             cout<<"  ";
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+
+    //     * 
+    //    * * 
+    //   *   * 
+    //  *     * 
+    // *       * 
+    // *       * 
+    //  *     *
+    //   *   *
+    //    * *
+    //     *
+    int num;
     cin>>num;
     int n=num/2;
     for(int row=0; row<n; row++){
-        //space
         for(int col=0; col<n-row-1; col++){
             cout<<" ";
         }
         for(int col=0; col<row+1; col++){
-            cout<<"* ";
+            if(col==0 || col==row+1-1){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
         }
         cout<<endl;
-        //start
     }
-
     for(int row=0; row<n; row++){
         for(int col=0; col<row; col++){
             cout<<" ";
         }
-        for(int col=0; col<n-row; col++){
-            cout<<"* ";
+        int totalCol = n-row;
+        for(int col=0; col<totalCol; col++){
+            if(col==0 || col==totalCol-1){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
         }
         cout<<endl;
     }
