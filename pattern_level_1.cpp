1180e33f9ea55a71ad161ac66fe81c7399d8db6f
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-19 12:09:04 +0530

    code updated

--- pattern_level_1.cpp
+++ pattern_level_1.cpp
@@ -58,7 +58,7 @@ int main(){
     // int n;
     // cin>>n;
     // for(int row=0; row<n; row++){
-    //     for(int col=0; col<row-1; col++){
+    //     for(int col=0; col<row+1; col++){
     //         cout<<"* ";
     //     }
     //     cout<<endl;
