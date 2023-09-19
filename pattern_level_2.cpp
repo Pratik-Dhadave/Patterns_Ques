24dbe907538d6dcaf2adc2f915a00f7b4f314554
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-19 12:07:14 +0530

    full pyramid, inverted full pyrammid & diagonal pattern done

---
+++ pattern_level_2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //     * 
+    //    * * 
+    //   * * * 
+    //  * * * * 
+    // * * * * * 
+
+    // int n;
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     //space
+    //     for(int col=0; col<n-row-1; col++){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0; col<row+1; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    //     //start
+    // }
+
+    // * * * * * 
+    //  * * * * 
+    //   * * * 
+    //    * * 
+    //     * 
+
+    // int n; 
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     for(int col=0; col<row; col++){
+    //         cout<<" ";
+    //     }
+    //     for(int col=0; col<n-row; col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    int num; 
+    cin>>num;
+    int n=num/2;
+    for(int row=0; row<n; row++){
+        //space
+        for(int col=0; col<n-row-1; col++){
+            cout<<" ";
+        }
+        for(int col=0; col<row+1; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        //start
+    }
+
+    for(int row=0; row<n; row++){
+        for(int col=0; col<row; col++){
+            cout<<" ";
+        }
+        for(int col=0; col<n-row; col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
